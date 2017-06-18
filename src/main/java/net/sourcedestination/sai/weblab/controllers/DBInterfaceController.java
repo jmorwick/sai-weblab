5797eb249aca4741730d40837f461f8ab59b0d4a
jmorwick <jbmorwick@gmail.com>
2017-06-17 19:45:36 -0500

    added a view for working with a single DBInterface, also prepared for possibly defining DBInterfaces at runtime via a command-line in the future (not sure if I'll follow through on this)

--- src/main/java/net/sourcedestination/sai/weblab/controllers/DBInterfaceController.java
+++ src/main/java/net/sourcedestination/sai/weblab/controllers/DBInterfaceController.java
@@ -1,5 +1,7 @@
 package net.sourcedestination.sai.weblab.controllers;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import net.sourcedestination.sai.db.DBInterface;
@@ -7,21 +9,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class MainPageController {
+public class DBInterfaceController {
 
     @Autowired
     private ApplicationContext appContext;
 
-    @GetMapping({"/","dbs"})
+    private Map<String,DBInterface> runtimeDbs = new HashMap<>();
+
+    @GetMapping({"/","/dbs"})
     public String view(Map<String, Object> model) {
         Map<String,DBInterface> dbs = appContext.getBeansOfType(DBInterface.class);
         Map<String,String> dbsinfo = dbs.entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey,
                         e -> e.getValue().getClass().getSimpleName()));
+        runtimeDbs.entrySet().stream()
+                .forEach(e -> dbsinfo.put(
+                        e.getKey(),
+                        e.getValue().getClass().getSimpleName()));
+
         model.put("dbs", dbsinfo);
         return "main";
+    }
+
+    @GetMapping({"/dbs/view/{dbname}"})
+    public String viewDB(Map<String, Object> model,
+                         @PathVariable("dbname") String dbname) {
+        DBInterface db = runtimeDbs.get(dbname);
+        if(db == null) db = (DBInterface)appContext.getBean(dbname);
+        model.put("dbname", dbname);
+        return "viewdb";
     }
 
 }
---
+++ src/main/resources/templates/viewdb.ftl
@@ -0,0 +1,4 @@
+<#include "header.ftl">
+<h1>DBInterface: ${dbname}</h1>
+
+<#include "footer.ftl">
