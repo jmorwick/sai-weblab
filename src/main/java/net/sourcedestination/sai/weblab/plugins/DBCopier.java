55f9aeb4bb1d1a31ddeb00e6a2c33e4e0267a6f7
jmorwick <jbmorwick@gmail.com>
2018-04-02 13:43:26 -0500

    added a task for copying one db to another

---
+++ src/main/java/net/sourcedestination/sai/weblab/plugins/DBCopier.java
@@ -0,0 +1,26 @@
+package net.sourcedestination.sai.weblab.plugins;
+
+import net.sourcedestination.sai.db.DBInterface;
+import net.sourcedestination.sai.graph.Graph;
+import net.sourcedestination.sai.task.DBPopulator;
+
+import java.util.stream.Stream;
+
+public class DBCopier extends DBPopulator {
+
+    private final DBInterface db;
+
+    public DBCopier(DBInterface db) {
+        this.db = db;
+    }
+
+    @Override
+    public Stream<Graph> getGraphStream() {
+        return db.getGraphIDStream().map(db::retrieveGraph);
+    }
+
+    @Override
+    public int getNumGraphs() {
+        return db.getDatabaseSize();
+    }
+}
