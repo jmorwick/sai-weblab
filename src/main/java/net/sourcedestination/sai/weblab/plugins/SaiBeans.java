5dadebdd80295de1395c2ebba2982a784e5e48fb
jmorwick <jbmorwick@gmail.com>
2018-05-08 11:43:29 -0500

    initial work on including classification task

--- src/main/java/net/sourcedestination/sai/weblab/plugins/SaiBeans.java
+++ src/main/java/net/sourcedestination/sai/weblab/plugins/SaiBeans.java
@@ -1,8 +1,11 @@
 package net.sourcedestination.sai.weblab.plugins;
 
+import net.sourcedestination.sai.graph.Feature;
+import net.sourcedestination.sai.learning.ClassificationModel;
+import net.sourcedestination.sai.learning.ClassificationModelGenerator;
 import net.sourcedestination.sai.retrieval.FeatureIndexBasedRetriever;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+
+import java.util.Optional;
 
 /**
  * Some static methods from the SAI library are intended to be used as method references
@@ -12,5 +15,18 @@ import org.apache.log4j.Logger;
 public class SaiBeans {
     public static final FeatureIndexBasedRetriever getRetreiveByBasicFeatureIndexCount() {
         return FeatureIndexBasedRetriever::retreiveByBasicFeatureIndexCount;
+    }
+
+    public static final ClassificationModel getCheatingClassifier() {
+        return  g -> {
+            Optional<Feature> res = g.getFeatures()
+                    .filter(f -> f.getName().equals("expected-classification")).findFirst();
+            return res.isPresent() ? res.get().getValue() : "";
+        };
+    }
+
+    public static final ClassificationModelGenerator getCheaterGenerator() {
+        return (db, trainingLabels) -> getCheatingClassifier();
+
     }
 }
---
+++ src/main/resources/templates/tasks/classification.ftl
@@ -0,0 +1,18 @@
+<form action="/tasks/classification" method="post" ID="classification-form">
+    <h3>Cross Validation Retrieval Experiment</h3>
+    <label>Data Source</label>
+    <select class="form-control" name="db">
+    <#list dbs as dbname, dbtype>
+        <option>${dbname}</option>
+    </#list>
+    </select>
+    <label>Classifier Generator</label>
+    <select class="form-control" name="classifergen">
+    <#list classifiers as classifiername>
+        <option>${classifiername}</option>
+    </#list>
+    </select>
+    <label>Folds</label>
+    <input class="form-control" type="text" name="folds"/>
+    <button type="submit" class="btn btn-primary">Launch Task</button>
+</form>
