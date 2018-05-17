e90349de60fc0d31d642d3683b5730f38954ea7e
jmorwick <jbmorwick@gmail.com>
2018-05-16 20:31:36 -0500

    updated report controller to work from internally stored reports
    will allow loading / saving from JSON as well

--- src/main/java/net/sourcedestination/sai/weblab/controllers/ReportsController.java
+++ src/main/java/net/sourcedestination/sai/weblab/controllers/ReportsController.java
@@ -1,17 +1,11 @@
 package net.sourcedestination.sai.weblab.controllers;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
-import net.sourcedestination.sai.db.DBInterface;
-import net.sourcedestination.sai.task.Task;
-import net.sourcedestination.sai.weblab.TaskManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -21,38 +15,19 @@ public class ReportsController {
     private ApplicationContext appContext;
 
     @Autowired
-    private TaskManager taskManager;
+    private TasksController tasksController;
 
     @GetMapping("/reports")
     public String view(Map<String, Object> model) {
 
-        Map<String,String> logs = new HashMap<>();
-        for(int tid : taskManager.getTrackedTaskIds()) {
-            Task t = taskManager.getTask(tid);
-        }
-        model.put("logs", logs);
+
         return "reports";
     }
 
-    @GetMapping("/reports/view/{taskid}")
+    @GetMapping("/reports/view/{reportid}")
     public String viewTask(Map<String, Object> model,
-                       @PathVariable("taskid") int taskid) {
-        model.put("taskid", ""+taskid);
+                       @PathVariable("reportid") int reportid) {
+        model.put("reportid", ""+reportid);
         return "viewlog";
     }
-
-    @GetMapping("/reports/view-query/{taskid}/{queryid}")
-    public String viewQuery(Map<String, Object> model,
-                            @PathVariable("taskid") int taskid,
-                            @PathVariable("queryid") int queryid) {
-
-        BiMap<String,DBInterface> dbs =
-                HashBiMap.create(appContext.getBeansOfType(DBInterface.class));
-
-        model.put("taskid", ""+taskid);
-        model.put("queryid", ""+queryid);
-        return "viewquery";
-    }
-
-
 }
