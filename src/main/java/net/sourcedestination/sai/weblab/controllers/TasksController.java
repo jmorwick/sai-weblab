8a6100bff1db00f534582ef3504df14c26bb7e94
jmorwick <jbmorwick@gmail.com>
2018-05-16 20:32:47 -0500

    consolidated task manager and task controller

--- src/main/java/net/sourcedestination/sai/weblab/controllers/TasksController.java
+++ src/main/java/net/sourcedestination/sai/weblab/controllers/TasksController.java
@@ -1,5 +1,6 @@
 package net.sourcedestination.sai.weblab.controllers;
 
+import com.google.common.collect.Sets;
 import net.sourcedestination.sai.analysis.GraphProcessor;
 import net.sourcedestination.sai.db.DBInterface;
 import net.sourcedestination.sai.graph.Graph;
@@ -16,8 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -29,9 +30,61 @@ public class TasksController {
     @Autowired
     private ApplicationContext appContext;
 
-    @Autowired
-    private TaskManager taskManager;
 
+    private int nextTaskId = 1;
+    private Map<Integer,Task> trackedTasks = new HashMap<>();
+    private Map<Integer,CompletableFuture> taskFutures = new HashMap<>();
+    private Map<Integer,Date> startTimes = new HashMap<>();
+    private Map<Integer,Date> endTimes = new HashMap<>();
+    private Map<Integer, Long> taskTimes = new HashMap<>();
+
+    public synchronized int addTask(Task t) {
+        final int id = nextTaskId;
+        final TasksController self = this;
+        startTimes.put(id, new Date());
+        trackedTasks.put(id, t);
+        long startTime = System.nanoTime();
+        CompletableFuture f = CompletableFuture.supplyAsync(t)
+                .thenApply(result -> { // record the result here when finished
+                    synchronized(self) {
+                        endTimes.put(id, new Date());
+                        taskTimes.put(id, (System.nanoTime() - startTime) / 1000000);
+                    }
+                    return result;
+                });
+        taskFutures.put(nextTaskId, f);
+        return nextTaskId++;
+    }
+
+    public synchronized Date getStartTime(int id) { return startTimes.get(id); }
+
+    public synchronized Date getEndTime(int id) { return endTimes.get(id); }
+
+    public synchronized Object getResult(int id) {
+        try {
+            return taskFutures.get(id).get();
+        } catch(Exception e) {
+            return null;
+        }
+    }
+
+    public synchronized Long getTaskTime(int id) { return taskTimes.get(id); }
+
+    public synchronized Task getTask(int id) {
+        return trackedTasks.get(id);
+    }
+
+    public synchronized Set<Integer> getTrackedTaskIds() {
+        return new HashSet<Integer>(trackedTasks.keySet());
+    }
+
+    public synchronized Set<Integer> getCompletedTaskIds() {
+        return new HashSet<Integer>(endTimes.keySet());
+    }
+
+    public synchronized Set<Integer> getRunningTaskIds() {
+        return Sets.difference(getTrackedTaskIds(), getCompletedTaskIds());
+    }
     @GetMapping("tasks")
     public String view(Map<String, Object> model) {
         Map<String,String> activeTasks = new HashMap<>();
@@ -44,22 +97,22 @@ public class TasksController {
         Map<String,String> results = new HashMap<>();
 
         // populate info for currently running tasks
-        for(int tid : taskManager.getRunningTaskIds()) {
-            Task t = taskManager.getTask(tid);
+        for(int tid : getRunningTaskIds()) {
+            Task t = getTask(tid);
             percentageComplete.put(""+tid, t.getPercentageDone());
             progress.put(""+tid, t.getProgressUnits());
             activeTasks.put(""+tid, ""+t.getTaskName());
-            startTimes.put(""+tid, taskManager.getStartTime(tid).toString());
+            startTimes.put(""+tid, getStartTime(tid).toString());
         }
 
         // populate info for completed tasks
-        for(int tid : taskManager.getCompletedTaskIds()) {
-            Task t = taskManager.getTask(tid);
+        for(int tid : getCompletedTaskIds()) {
+            Task t = getTask(tid);
             inactiveTasks.put(""+tid, ""+t.getTaskName());
-            startTimes.put(""+tid, taskManager.getStartTime(tid).toString());
-            endTimes.put(""+tid, taskManager.getEndTime(tid).toString());
-            taskTimes.put(""+tid, ""+(taskManager.getEndTime(tid).getTime() - taskManager.getStartTime(tid).getTime()));
-            Object result = taskManager.getResult(tid);
+            startTimes.put(""+tid, getStartTime(tid).toString());
+            endTimes.put(""+tid, getEndTime(tid).toString());
+            taskTimes.put(""+tid, ""+(getEndTime(tid).getTime() - getStartTime(tid).getTime()));
+            Object result = getResult(tid);
             results.put(""+tid, result == null ? "n/a" : result.toString());
         }
 
@@ -130,7 +183,7 @@ public class TasksController {
             query = null;
         }
 
-        taskManager.addTask(new Task() {
+        addTask(new Task() {
             private AtomicInteger progress = new AtomicInteger(0);
             public Object get() {
                 retriever.retrieve(db,query)
