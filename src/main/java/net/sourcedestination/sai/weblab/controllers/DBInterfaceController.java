ebdd8b82500b9a71084415e4149fc26b018461b0
jmorwick <jbmorwick@gmail.com>
2018-05-23 12:18:45 -0500

    added processor to show retrieved graph id's
    view will have links to retrieve full graph for each

--- src/main/java/net/sourcedestination/sai/weblab/controllers/DBInterfaceController.java
+++ src/main/java/net/sourcedestination/sai/weblab/controllers/DBInterfaceController.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class DBInterfaceController {
-    
+
     @Autowired
     private ApplicationContext appContext;
 
---
+++ src/main/java/net/sourcedestination/sai/weblab/plugins/RetrievedGraphsList.java
@@ -0,0 +1,37 @@
+package net.sourcedestination.sai.weblab.plugins;
+
+import net.sourcedestination.sai.analysis.ExperimentLogProcessor;
+import net.sourcedestination.sai.analysis.ExperimentLogProcessorFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class RetrievedGraphsList implements ExperimentLogProcessor {
+
+    private List<List> retrievedGraphIds = new ArrayList<>();
+
+    @Override
+    public String getPattern() {
+        return "retrieved Graph ID #(\\d+) from (.+)$";
+    }
+
+    @Override
+    public void processLogMessage(String ... groups) {
+        retrievedGraphIds.add(List.of(Integer.parseInt(groups[1]), groups[2]));
+    }
+
+    @Override
+    public Map<String,Object> get() {
+        Map<String,Object> model = new HashMap<>();
+        model.put("retrieved-graphs", retrievedGraphIds);
+        return model;
+    }
+
+    public static ExperimentLogProcessorFactory getFactory() {
+        return RetrievedGraphsList::new;
+    }
+
+}
